f134b837ca04bcdaeda9363e6bcb5fd9a420b4d1
Les Carbonaro <lcarbonaro@gmail.com>
2022-05-24 22:44:38 -0400

    add API calls

--- src/App.vue
+++ src/App.vue
@@ -74,7 +74,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 .text-gradient {
   background: linear-gradient(to right, #30d08d, #c43ad6) !important;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,127 @@
-
-
 <template>
-   <q-page class="flex flex-center">
-    HOME
-  </q-page>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm row">
+
+      <div id="divLeft" class="col-4">
+        <div>
+          <q-input v-model="inpWalletAddr" placeholder="Wallet address" dense>
+            <template v-slot:append>
+              <q-icon name="close" @click="inpWalletAddr = ''" class="cursor-pointer" />
+            </template>
+          </q-input>
+        </div>
+
+        <div style="margin:15px 0;">
+          <q-input v-model="inpCollectionAddr" placeholder="Collection address" dense>
+            <template v-slot:append>
+              <q-icon name="close" @click="inpCollectionAddr = ''" class="cursor-pointer" />
+            </template>
+          </q-input>
+
+        </div>
+
+        <div style="margin:15px 0;">
+          <q-checkbox left-label v-model="chkCollection" checked="chkCollection" label="Fetch for collection" />
+        </div>
+
+        <div style="margin:15px 0;">
+          <q-btn :loading="isLoading" flat rounded class="btn-gradient" label="Let's Go" @click="callAPI" />
+        </div>
+
+      </div>
+
+      <div id="divRight" class="col-6" style="margin-left:25px;">
+        RESULTS
+
+      </div>
+
+
+    </div>
+
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const chkCollection = ref(false);
+const inpWalletAddr = ref('');
+const inpCollectionAddr = ref('');
+const isLoading = ref(false);
+
+const nfts = ref([]);
+
+const apiKey = "hBfRT-PTexTchntBso2fOt6GKfSCGuHD";  
+
+async function callAPI() {
+  if( chkCollection.value ) {
+    await getNFTsByCollection();
+  } else {
+    await getNFTs();
+  }
+}
+
+async function getNFTsByCollection() {
+  
+  isLoading.value = true;
+
+  
+  const baseURL = `https://polygon-mumbai.g.alchemy.com/v2/${apiKey}/getNFTsForCollection/`;
+  const requestOptions = { method: 'GET' };
+
+  let fetchURL;
+  
+  if (inpCollectionAddr.value.length) {
+    fetchURL = `${baseURL}?contractAddress=${inpCollectionAddr.value}&withMetadata=${"true"}`;    
+  } else {   
+    // ??
+  }
+
+  let resp = await fetch(fetchURL, requestOptions);
+  let respJson = await resp.json();
+
+  if (respJson) {    
+    nfts.value = respJson.nfts;
+  }
+
+  isLoading.value = false;
+  
+}  // getNFTsByCollection
+
+async function getNFTs() {
+  //console.log('chkColl',chkCollection.value);
+
+  isLoading.value = true;
+
+  const apiKey = "hBfRT-PTexTchntBso2fOt6GKfSCGuHD";  
+  const baseURL = `https://polygon-mumbai.g.alchemy.com/v2/${apiKey}/getNFTs/`;
+  const requestOptions = { method: 'GET' };
+
+  let fetchURL;
+   
+  if (!inpCollectionAddr.value) {
+    fetchURL = `${baseURL}?owner=${inpWalletAddr.value}`;    
+  } else {   
+    fetchURL = `${baseURL}?owner=${inpWalletAddr}&contractAddresses%5B%5D=${inpCollectionAddr.value}`;    
+  }
+
+  let resp = await fetch(fetchURL, requestOptions);
+  let respJson = await resp.json();
+
+  if (respJson) {    
+    nfts.value = respJson.ownedNfts;
+  }
+
+  isLoading.value = false;
+  
+}  // getNFTs
 
 </script>
+
+<style scoped>
+.btn-gradient {
+  background-image: linear-gradient(to right, #30d08d, #c43ad6) !important;
+  color: white !important;
+  margin-left: 20px;
+}
+</style>
