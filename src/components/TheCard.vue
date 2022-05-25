a88489c974c2f7ff64b029dcf40e263c3b1c0c33
Les Carbonaro <lcarbonaro@gmail.com>
2022-05-25 10:47:29 -0400

    Add TheCard component

---
+++ src/components/TheCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-card class="my-card">
+      <img :src="image">
+
+      <q-card-section>
+        <div class="text-h6">{{title}}</div>
+        <div class="text-subtitle2">{{subtitle}}</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ullam?
+      </q-card-section>
+    </q-card>
+
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  subtitle: String,
+  image: String
+})
+
+</script>
+
+
+<style scoped>
+.my-card {
+  width: 100%;
+  max-width: 250px;
+}
+</style>
